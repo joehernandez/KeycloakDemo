363408e38c0b3cb3c42417d66e3ce4947e5abf91
Joe Hernandez <jhernandez@papertransport.com>
2024-01-11 11:15:36 -0600

    Add endpoint with Authorize attribute

--- KeycloakDemo.Api/Controllers/WeatherForecastController.cs
+++ KeycloakDemo.Api/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KeycloakDemo.Api.Controllers;
@@ -19,6 +20,19 @@ public class WeatherForecastController : ControllerBase
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [Authorize]
+    [HttpGet(Name = "GetWeatherForecastWithAuth")]
+    public IEnumerable<WeatherForecast> GetWithAuth()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
